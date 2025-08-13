8dc20f33515bfe8af74d593ae7342b45cc9b67f6
jmanhype <straughterguthrie@gmail.com>
2025-08-13 14:00:26 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/core/crdt/context_graph.ex

--- lib/cybernetic/core/crdt/context_graph.ex
+++ lib/cybernetic/core/crdt/context_graph.ex
@@ -1,10 +1,58 @@
 
-defmodule Cybernetic.Context.Graph do
+defmodule Cybernetic.Core.CRDT.ContextGraph do
   @moduledoc """
-  Delta-CRDT powered semantic context store.
+  Delta CRDT-backed semantic context graph for entity/predicate/object triples with causal metadata.
+  Provides distributed state synchronization across VSM systems.
   """
-  def start_link() do
-    DeltaCrdt.start_link(DeltaCrdt.AWLWWMap, crdt_options())
+  use GenServer
+  require Logger
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
-  defp crdt_options, do: [sync_interval: 25, ship_interval: 25, ship_debounce: 5]
+
+  def init(_opts) do
+    {:ok, crdt} = DeltaCrdt.start_link(
+      DeltaCrdt.AWLWWMap, 
+      sync_interval: 50, 
+      ship_interval: 50, 
+      ship_debounce: 10
+    )
+    {:ok, %{crdt: crdt}}
+  end
+
+  @doc """
+  Store a semantic triple (subject, predicate, object) with metadata.
+  """
+  @spec put_triple(term(), term(), term(), map()) :: :ok
+  def put_triple(subject, predicate, object, meta \\ %{}) do
+    GenServer.cast(__MODULE__, {:put_triple, subject, predicate, object, meta})
+  end
+
+  @doc """
+  Query triples by subject, predicate, or object.
+  """
+  @spec query(subject: term() | nil, predicate: term() | nil, object: term() | nil) :: list()
+  def query(criteria) do
+    GenServer.call(__MODULE__, {:query, criteria})
+  end
+
+  def handle_cast({:put_triple, subject, predicate, object, meta}, %{crdt: crdt} = state) do
+    key = :erlang.term_to_binary({subject, predicate, object})
+    value = %{
+      subject: subject, 
+      predicate: predicate, 
+      object: object, 
+      meta: Map.merge(%{timestamp: System.system_time(:millisecond)}, meta)
+    }
+    DeltaCrdt.mutate(crdt, :add, [key, value])
+    {:noreply, state}
+  end
+
+  def handle_call({:query, _criteria}, _from, %{crdt: crdt} = state) do
+    # Simple implementation - get all values for now
+    # Could be optimized with indexing
+    values = DeltaCrdt.read(crdt) |> Map.values()
+    {:reply, values, state}
+  end
 end
