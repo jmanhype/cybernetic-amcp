27dee6c530c5fe1208cd1cf75802d75478ab4550
jmanhype <straughterguthrie@gmail.com>
2025-08-13 11:41:29 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/config/test.exs

--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,10 @@
 import Config
+
+# Configure to use InMemory transport during tests
+config :cybernetic,
+  transport: Cybernetic.Transport.InMemory,
+  test_mode: true
+
+# Disable AMQP during tests
+config :cybernetic, :amqp,
+  enabled: false
