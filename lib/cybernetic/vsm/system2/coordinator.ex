0b0fcbe776e3ee39b86411b58ee8a659e7f75cfc
jmanhype <straughterguthrie@gmail.com>
2025-08-13 11:29:42 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/vsm/system2/coordinator.ex

--- lib/cybernetic/vsm/system2/coordinator.ex
+++ lib/cybernetic/vsm/system2/coordinator.ex
@@ -20,7 +20,7 @@ defmodule Cybernetic.VSM.System2.Coordinator do
   def handle_cast({:transport_message, message, opts}, state) do
     # Route message to the appropriate message handler
     operation = Map.get(message, "operation", "unknown")
-    meta = Map.get(opts, :meta, %{})
+    meta = Keyword.get(opts, :meta, %{})
     
     # Process the message through the message handler
     Cybernetic.VSM.System2.MessageHandler.handle_message(operation, message, meta)
