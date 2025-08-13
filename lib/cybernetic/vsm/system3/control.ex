de70e1f8b25c7543e6ae9c384dd2fd3d17828990
jmanhype <straughterguthrie@gmail.com>
2025-08-13 11:24:10 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/vsm/system3/control.ex

--- lib/cybernetic/vsm/system3/control.ex
+++ lib/cybernetic/vsm/system3/control.ex
@@ -6,4 +6,22 @@ defmodule Cybernetic.VSM.System3.Control do
   """
   def start_link(opts \\ []), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   def init(state), do: {:ok, Map.merge(%{metrics: %{}, policies: %{}}, state)}
+  
+  # Handle transport messages from in-memory transport
+  def handle_cast({:transport_message, message, opts}, state) do
+    # Route message to the appropriate message handler
+    operation = Map.get(message, "operation", "unknown")
+    meta = Map.get(opts, :meta, %{})
+    
+    # Process the message through the message handler
+    Cybernetic.VSM.System3.MessageHandler.handle_message(operation, message, meta)
+    
+    {:noreply, state}
+  end
+  
+  # Test interface - routes messages through the message handler
+  def handle_message(message, meta \\ %{}) do
+    operation = Map.get(message, :operation, "unknown")
+    Cybernetic.VSM.System3.MessageHandler.handle_message(operation, message, meta)
+  end
 end
