95eb60ad0a067b417b560148856aa8ab18283522
jmanhype <straughterguthrie@gmail.com>
2025-08-13 09:46:30 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/vsm/system4/intelligence.ex

--- lib/cybernetic/vsm/system4/intelligence.ex
+++ lib/cybernetic/vsm/system4/intelligence.ex
@@ -6,4 +6,10 @@ defmodule Cybernetic.VSM.System4.Intelligence do
   """
   def start_link(opts \\ []), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   def init(state), do: {:ok, state}
+  
+  # Test interface - routes messages through the message handler
+  def handle_message(message, meta \\ %{}) do
+    operation = Map.get(message, :operation, "unknown")
+    Cybernetic.VSM.System4.MessageHandler.handle_message(operation, message, meta)
+  end
 end
