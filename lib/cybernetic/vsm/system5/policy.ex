abb5d0bce54d1395e28076c3f9e12af2f6aead02
jmanhype <straughterguthrie@gmail.com>
2025-08-13 11:24:36 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/vsm/system5/policy.ex

--- lib/cybernetic/vsm/system5/policy.ex
+++ lib/cybernetic/vsm/system5/policy.ex
@@ -7,4 +7,22 @@ defmodule Cybernetic.VSM.System5.Policy do
 
   def start_link(opts \\ []), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   def init(state), do: {:ok, Map.put(state, :identity, %{name: "Cybernetic"})}
+  
+  # Handle transport messages from in-memory transport
+  def handle_cast({:transport_message, message, opts}, state) do
+    # Route message to the appropriate message handler
+    operation = Map.get(message, "operation", "unknown")
+    meta = Map.get(opts, :meta, %{})
+    
+    # Process the message through the message handler
+    Cybernetic.VSM.System5.MessageHandler.handle_message(operation, message, meta)
+    
+    {:noreply, state}
+  end
+  
+  # Test interface - routes messages through the message handler
+  def handle_message(message, meta \\ %{}) do
+    operation = Map.get(message, :operation, "unknown")
+    Cybernetic.VSM.System5.MessageHandler.handle_message(operation, message, meta)
+  end
 end
