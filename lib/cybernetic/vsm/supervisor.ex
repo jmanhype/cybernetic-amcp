cba8bc6a15d2abc890671ac54bb4045919bb33ea
jmanhype <straughterguthrie@gmail.com>
2025-08-13 12:23:45 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/vsm/supervisor.ex

--- lib/cybernetic/vsm/supervisor.ex
+++ lib/cybernetic/vsm/supervisor.ex
@@ -10,16 +10,16 @@ defmodule Cybernetic.VSM.Supervisor do
   def init(_opts) do
     children = [
       # S5 Policy/Identity
-      Cybernetic.VSM.System5.Policy,
+      {Cybernetic.VSM.System5.Policy, restart: :permanent},
       # S4 Intelligence
-      Cybernetic.VSM.System4.Intelligence,
+      {Cybernetic.VSM.System4.Intelligence, restart: :permanent},
       # S3 Control
-      Cybernetic.VSM.System3.Control,
+      {Cybernetic.VSM.System3.Control, restart: :permanent},
       # S2 Coordination
-      Cybernetic.VSM.System2.Coordinator,
+      {Cybernetic.VSM.System2.Coordinator, restart: :permanent},
       # S1 Operations
-      Cybernetic.VSM.System1.Operational
+      {Cybernetic.VSM.System1.Operational, restart: :permanent}
     ]
-    Supervisor.init(children, strategy: :rest_for_one)
+    Supervisor.init(children, strategy: :rest_for_one, max_restarts: 10, max_seconds: 60)
   end
 end
