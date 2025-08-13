101f6831976838bbb0124acf8f57e07709e3a953
jmanhype <straughterguthrie@gmail.com>
2025-08-13 14:54:24 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/apps/telegram/infra/service.ex

--- lib/cybernetic/apps/telegram/infra/service.ex
+++ lib/cybernetic/apps/telegram/infra/service.ex
@@ -3,5 +3,5 @@ defmodule Cybernetic.Telegram.Infra.Service do
   @moduledoc """
   HTTP bridge/webhook placeholder; add real Telegram client.
   """
-  def push_update(update), do: GenServer.cast(Cybernetic.Telegram.Agent, {:incoming_message, update})
+  def push_update(update), do: GenServer.cast(Cybernetic.VSM.System1.Agents.TelegramAgent, {:incoming_message, update})
 end
