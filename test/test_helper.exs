f41f511f6583f5bfd2564b7eb6f1aa767876bc89
jmanhype <straughterguthrie@gmail.com>
2025-08-13 11:09:53 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/test/test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,7 @@
 # Set test mode to prevent feedback loops
 Application.put_env(:cybernetic, :test_mode, true)
 
+# Use in-memory transport for tests
+Application.put_env(:cybernetic, :transport, Cybernetic.Transport.InMemory)
+
 ExUnit.start()
