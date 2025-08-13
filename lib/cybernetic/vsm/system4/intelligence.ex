429e13616b76ac33f9ec3b098fb8fc96d4daade3
jmanhype <straughterguthrie@gmail.com>
2025-08-13 11:24:24 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/vsm/system4/intelligence.ex

--- lib/cybernetic/vsm/system4/intelligence.ex
+++ lib/cybernetic/vsm/system4/intelligence.ex
@@ -7,6 +7,18 @@ defmodule Cybernetic.VSM.System4.Intelligence do
   def start_link(opts \\ []), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   def init(state), do: {:ok, state}
   
+  # Handle transport messages from in-memory transport
+  def handle_cast({:transport_message, message, opts}, state) do
+    # Route message to the appropriate message handler
+    operation = Map.get(message, "operation", "unknown")
+    meta = Map.get(opts, :meta, %{})
+    
+    # Process the message through the message handler
+    Cybernetic.VSM.System4.MessageHandler.handle_message(operation, message, meta)
+    
+    {:noreply, state}
+  end
+  
   # Test interface - routes messages through the message handler
   def handle_message(message, meta \\ %{}) do
     operation = Map.get(message, :operation, "unknown")
