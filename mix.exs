881c4317d6a9d551dc59d1f811f6487d30267e64
jmanhype <straughterguthrie@gmail.com>
2025-08-14 08:39:22 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/mix.exs

--- mix.exs
+++ mix.exs
@@ -46,8 +46,8 @@ defmodule Cybernetic.MixProject do
       {:bloomex, "~> 1.0"},  # Bloom filter for replay protection
       {:nanoid, "~> 2.0"},    # Nonce generation
       
-      # WASM runtime
-      {:wasmex, "~> 0.9.2"},
+      # WASM runtime (using older version for rustler compatibility)
+      {:wasmex, "~> 0.8.3"},
       
       # HTTP client for Claude API
       {:req, "~> 0.5.0"},
