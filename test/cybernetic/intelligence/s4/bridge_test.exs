89c4ff9cdad8f8e24b9d1f4abfd32819316c1214
jmanhype <straughterguthrie@gmail.com>
2025-08-14 09:35:25 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/test/cybernetic/intelligence/s4/bridge_test.exs

--- test/cybernetic/intelligence/s4/bridge_test.exs
+++ test/cybernetic/intelligence/s4/bridge_test.exs
@@ -135,7 +135,6 @@ defmodule Cybernetic.Intelligence.S4.BridgeTest do
   end
 
   describe "SOP forwarding" do
-    @tag :skip
     test "forwards analysis to SOP Engine when available" do
       # Start SOP Engine or get existing
       sop_pid = case Process.whereis(Cybernetic.Intelligence.S4.SOPEngine) do
