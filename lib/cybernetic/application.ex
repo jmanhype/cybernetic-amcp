0ef286a07d8f8ff4b4b2186f103b8799c17918d2
jmanhype <straughterguthrie@gmail.com>
2025-08-14 22:48:10 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/application.ex

--- lib/cybernetic/application.ex
+++ lib/cybernetic/application.ex
@@ -48,6 +48,9 @@ defmodule Cybernetic.Application do
       # S4 Multi-Provider Intelligence Service
       {Cybernetic.VSM.System4.Service, []},
       
+      # S3 Rate Limiter for budget management
+      {Cybernetic.VSM.System3.RateLimiter, []},
+      
       # Edge WASM Validator is stateless - use Cybernetic.Edge.WASM.Validator.load/2 where needed
       
       # VSM Supervisor (includes S1-S5)
