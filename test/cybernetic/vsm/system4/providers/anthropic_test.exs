bc96314cdf5e08a44f82b22a65819542c211b70a
jmanhype <straughterguthrie@gmail.com>
2025-08-16 16:47:17 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/test/cybernetic/vsm/system4/providers/anthropic_test.exs

--- test/cybernetic/vsm/system4/providers/anthropic_test.exs
+++ test/cybernetic/vsm/system4/providers/anthropic_test.exs
@@ -203,7 +203,7 @@ defmodule Cybernetic.VSM.System4.Providers.AnthropicTest do
           :counters.add(call_count, 1, 1)
           
           case count do
-            0 -> {:ok, %{status: 500, body: "Internal Server Error", headers: []}}
+            0 -> {:ok, %HTTPoison.Response{status_code: 500, body: "Internal Server Error", headers: []}}
             _ -> {:ok, %{status: 200, body: Jason.encode!(mock_response), headers: []}}
           end
         end
