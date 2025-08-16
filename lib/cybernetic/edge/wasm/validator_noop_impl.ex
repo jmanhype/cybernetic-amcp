49eee0e9e1b82742187609e25ed0fa3ce6345caa
jmanhype <straughterguthrie@gmail.com>
2025-08-16 00:19:02 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/edge/wasm/validator_noop_impl.ex

--- lib/cybernetic/edge/wasm/validator_noop_impl.ex
+++ lib/cybernetic/edge/wasm/validator_noop_impl.ex
@@ -4,5 +4,5 @@ defmodule Cybernetic.Edge.WASM.Validator.NoopImpl do
   @impl true
   def load(_bytes, _opts), do: {:ok, :noop}
   @impl true
-  def validate(_inst, _message, _opts), do: :ok
+  def validate(_inst, _message, _opts), do: {:ok, %{valid: true, noop: true}}
 end
