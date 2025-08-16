12282ae06cdb10d7dbc27498b9a71a1731020b8b
jmanhype <straughterguthrie@gmail.com>
2025-08-16 09:05:15 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/test/cybernetic/edge/wasm/validator_test.exs

--- test/cybernetic/edge/wasm/validator_test.exs
+++ test/cybernetic/edge/wasm/validator_test.exs
@@ -3,6 +3,6 @@ defmodule Cybernetic.Edge.WASMValidatorNoopTest do
 
   test "noop impl loads and validates" do
     {:ok, inst} = Cybernetic.Edge.WASM.Validator.load(<<0,0,0>>, fuel: 10)
-    assert {:ok, %{valid: true, noop: true}} == Cybernetic.Edge.WASM.Validator.validate(inst, %{"x" => 1})
+    assert {:ok, %{valid: true, noop: true}} = Cybernetic.Edge.WASM.Validator.validate(inst, %{"x" => 1})
   end
 end
