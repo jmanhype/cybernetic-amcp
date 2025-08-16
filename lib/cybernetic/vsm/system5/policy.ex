51d42fc26263fc4f02d4f1265b766282894ae539
jmanhype <straughterguthrie@gmail.com>
2025-08-16 11:25:46 -0500

    🔖 Checkpoint: Edit /Users/speed/Downloads/cybernetic/lib/cybernetic/vsm/system5/policy.ex

--- lib/cybernetic/vsm/system5/policy.ex
+++ lib/cybernetic/vsm/system5/policy.ex
@@ -5,7 +5,7 @@ defmodule Cybernetic.VSM.System5.Policy do
   S5: Identity/goal setting + meta-system spawning.
   """
 
-  def start_link(opts \\ []), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  def start_link(_opts \\ []), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   
   def init(state) do
     {:ok, state
